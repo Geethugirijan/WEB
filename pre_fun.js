e6e68a4b18b67939e8c870fe5fbf70a90e60c283
Geethugirijan <geethugirijan234@gmail.com>
2025-01-01 13:53:43 +0530

    Add files via upload

---
+++ pre_fun.js
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<body>
+<center>
+<h2><u><font color="blue">string methods</u></h2>
+</center>
+<center>
+<p><b>Java script string methods</b></p>
+<script>var name="i am arundathi m p";
+document.write(name);
+document.write("<br>");
+document.write("length of string is:");
+document.write(name.length);
+document.write("<br>");
+document.write("index of first occurence of a in string is:");
+document.write(name.indexOf("a"));
+document.write("<br>");
+document.write("index of last occurence of a in string is :");
+document.write(name.lastIndexOf("a"));
+document.write("<br>");
+document.write("index of am in string:");
+document.write(name.search("am"));
+document.write("<br>");
+
+</script>
+</center>
+</body>
+</html>
